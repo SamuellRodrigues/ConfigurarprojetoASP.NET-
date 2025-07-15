b3bddad345c36bc927ca9ce1a891b6baddf5fc55
SamuellRodrigues <baurukigamer@gmail.com>
2025-07-15 13:56:25 -0300

    ProjetoAspNet

---
+++ ConfigurarprojetoASP.NET-/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    //DbSets personalizados, se quiser adicionar depois
+    public DbSet<Pessoa> Pessoas { get; set; }
+}
+public class Pessoa
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
